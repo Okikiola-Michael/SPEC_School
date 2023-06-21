7544cfc7a9a48427215dd8adfd7dc8af131a2fc6
Okikiola-Michael <alegbeleyeokiki@gmail.com>
2023-06-21 07:44:50 -0400

    Added lines of code to predict AGB

--- StationC/test_folder/test_code_bc.R
+++ StationC/test_folder/test_code_bc.R
@@ -48,13 +48,18 @@ ggplot(MLBS.data) +
 
 # Relationship between AGB and other vars ---------------------------------
 AGB.data <- MLBS.data %>% select(AGB,
-                                 eastness_mean, northness_mean, slope_mean, tpi_mean, tri_mean, dsm_mean,
+                                 eastness_mean, northness_mean, 
+                                 slope_mean, tpi_mean, tri_mean, dsm_mean,
                                  latitude, longitude,
                                  PRI_mean, NDVI_mean, lai_mean)
 
 c.agb <- corrplot(cor(AGB.data))
 c.agb$corr[1,]
 
+#or 
+
+c.agb <- ggcorrplot(cor(AGB.data), lab = T) #makes more sense when you zoom the plot
+c.agb
 
 # Relationship between diversity and other vars ---------------------------
 Div.data <- MLBS.data %>% select(diversity_shannon,
@@ -64,3 +69,52 @@ Div.data <- MLBS.data %>% select(diversity_shannon,
 
 c.div <- corrplot(cor(Div.data))
 c.div$corr[1,]
+
+c.div <- ggcorrplot(cor(Div.data), lab = T) #makes more sense when you zoom the plot
+c.div
+
+
+# Selecting the best model to predict AGB
+
+best_model <- regsubsets(AGB ~ chm_mean + 
+                     convexHull + dtm_mean + lai_mean +  
+                    NDVI_mean + NIR_mean + pc1_mean + 
+                    pc2_mean + porosity_ratio_mean + 
+                    PRI_mean + RVSI_mean + slope_mean + 
+                    SWIR1_mean + SWIR2_mean + slope_mean + 
+                    tpi_mean + tri_mean + dsm_mean, nvmax = 1, 
+                   nbest = 7, data = MLBS.data)
+
+mod_summ = with(summary(best_model), data.frame(rsq, adjr2, cp, rss, bic, outmat))
+View(mod_summ)
+
+# The best predictor is the mean canopy height metrics with R-squared value of 93%
+cor(AGB, Predict)
+lm1 <- lm(AGB ~ chm_mean, data = MLBS.data)
+summary(lm1)
+
+MLBS.data$Pred <- fitted(lm1) #predicted values
+MLBS.data$Resid2 <- residuals(lm1) #residuals from the model
+
+
+# Diagnostic tests and plots
+
+plot1 <- ggplot(MLBS.data, aes(x =  Pred, y = Resid2)) + 
+  geom_point(cex = 2, pch = 1, color ="red") + 
+  theme_bw() +
+  labs(x = "Fitted Values", y = "Residuals") + 
+  geom_hline(yintercept = 0, col = "blue") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+plot2 = ggplot(MLBS.data, aes(x = AGB, y = Pred)) + 
+  geom_point(cex = 2, pch = 1, color ="red") + theme_bw() +
+  labs(x = "Observed AGB", y = "Fitted AGB") + 
+  geom_abline(intercept = 0, slope = 1, col = "blue") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+grid.arrange( plot1, plot2, ncol =2)
+
+
+
+
